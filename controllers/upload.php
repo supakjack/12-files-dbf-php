c5c1457578aff483be6463fd3af3543d2c2ef728
Supak Pukdam <48548611+supakjack@users.noreply.github.com>
2021-10-15 16:28:00 +0700

    add
    add

--- controllers/upload.php
+++ controllers/upload.php
@@ -35,11 +35,11 @@ for ($i = 0; $i < $total_count; $i++) {
                         if ($index > 0) {
                             $columns = explode('|', $members);
                             dbase_add_record($db, array(
-                                $columns[0],
-                                $columns[1],
-                                $columns[2],
-                                $columns[3],
-                                $columns[4],
+                                iconv("UTF-8", "tis-620", $columns[0]),
+                                iconv("UTF-8", "tis-620", $columns[1]),
+                                iconv("UTF-8", "tis-620", $columns[2]),
+                                iconv("UTF-8", "tis-620", $columns[3]),
+                                iconv("UTF-8", "tis-620", $columns[4]),
                             ));
                         }
                         $index++;
@@ -47,24 +47,24 @@ for ($i = 0; $i < $total_count; $i++) {
                 }
                 fclose($file);
                 dbase_close($db);
-            }else if ($_FILES['fileToUpload']['name'][$i] == 'CHTyymm.txt') {
+            } else if ($_FILES['fileToUpload']['name'][$i] == 'CHTyymm.txt') {
                 $def = array(
                     array("HN",     "C", 128),
                     array("AN",     "C", 128),
                     array("DATE",      "D"),
                     array("TOTAL",    "N", 10, 0),
                     array("PAID", "N", 10, 0),
-                    array("PTTYPE", "C",128)
+                    array("PTTYPE", "C", 128)
                 );
-            
+
                 if (!dbase_create('./test-CHTyymm.dbf', $def)) {
                     echo "Error, can't create the database\n";
                 }
-            
+
                 $file = fopen("./../uploads/" . $new_path . "/CHTyymm.txt", "r");
                 $db = dbase_open('./test-CHTyymm.dbf', 2);
                 $index = 0;
-            
+
                 while (!feof($file)) {
                     $line_of_text = fgets($file);
                     if (!ctype_space($line_of_text) && $line_of_text != '') {
@@ -78,8 +78,42 @@ for ($i = 0; $i < $total_count; $i++) {
                                 iconv("UTF-8", "tis-620", $columns[3]),
                                 iconv("UTF-8", "tis-620", $columns[4]),
                                 iconv("UTF-8", "tis-620", $columns[5]),
-                                
+
                             ));
+                        }
+                        $index++;
+                    }
+                }
+            } else if ($_FILES['fileToUpload']['name'][$i] == 'IDXyymm.txt') {
+                $def = array(
+                    array("HN",     "C", 128),
+                    array("DIAG",     "C", 128),
+                    array("DXTYPE",      "C", 128),
+                    array("DRDX",    "C", 128)
+                );
+
+                if (!dbase_create('./test-IDXyymm.dbf', $def)) {
+                    echo "Error, can't create the database\n";
+                }
+
+                $file = fopen("./../uploads/" . $new_path . "/IDXyymm.txt", "r");
+                $db = dbase_open('./test-IDXyymm.dbf', 2);
+                $index = 0;
+
+                while (!feof($file)) {
+                    $line_of_text = fgets($file);
+                    if (!ctype_space($line_of_text) && $line_of_text != '') {
+                        $members = $line_of_text;
+                        if ($index > 0) {
+                            $columns = explode('|', $members);
+                            echo $columns[3] . "<br>";
+                            dbase_add_record($db, array(
+                                iconv("UTF-8", "tis-620", $columns[0]),
+                                iconv("UTF-8", "tis-620", $columns[1]),
+                                iconv("UTF-8", "tis-620", $columns[2]),
+                                iconv("UTF-8", "tis-620", $columns[3] . substr(0, 1) . $columns[3] . substr(1, strlen($columns[3])))
+                            ));
+                            echo $columns[3] . substr(0, 1) . $columns[3] . substr(1, strlen($columns[3]))."<br>";
                         }
                         $index++;
                     }
@@ -88,6 +122,3 @@ for ($i = 0; $i < $total_count; $i++) {
         }
     }
 }
-
-
-
